f6b8c431576495ee207191ad3812f9e36640a1e0
Nikita Jadhav <nikita.jadhav12022008@gmail.com>
2026-03-04 05:22:39 -0800

    Code Updated

--- library_management.py
+++ library_management.py
@@ -1,38 +1,26 @@
+#python based library management system:
+#defining library as class:
 class library:
+    #defining constructor:
     def __init__(self,num,books):
         self.no_of_books = num
         self.books = books
-
+#defining function to show details:
     def show(self):
         print(f"there are {self.no_of_books} books and they are {self.books}")
-
+#defining function for checking: 
     def check(self):
         if(self.no_of_books != len(self.books)):
             print("no of books is incorrect!!")
         else:
             print("yep!! totally correct")
-
+#object defining and fuction calling:
 a = library(3,["harry potter",'got','fire and blood'])
 a.show()
 a.check()
 
-class library:
-    def __init__(self,num,books):
-        self.no_of_books = num
-        self.books = books
 
-    def show(self):
-        print(f"there are {self.no_of_books} books and they are {self.books}")
 
-    def check(self):
-        if(self.no_of_books != self.books):
-            print("no of books is incorrect")
-        else:
-            print("yep!! totally correct")
-
-a = library(5,["harry potter","got","fire and blood"])
-a.show()
-a.check()
 
 
 
